c4f85aaed4a278e3c37645eb7d4b62632cc511ea
Luis Felipe Santofimio Buitrago <luisfelipesantofimio@gmail.com>
2024-07-03 18:40:12 -0500

    Fixed workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   site: "https://luisfelipesantofimio.github.io",
   base: "aronnax-erm",
   i18n: {
